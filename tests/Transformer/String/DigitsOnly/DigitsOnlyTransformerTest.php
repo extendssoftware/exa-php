95e5bec963a484bf0bf45f2c49d5562eff6f28ce
vincentvandijk <info@vincentvandijk.com>
2026-03-18 07:29:29 +0100

    Added DigitsOnlyTransformer to transform a string to digits.

---
+++ tests/Transformer/String/DigitsOnly/DigitsOnlyTransformerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ExtendsSoftware\ExaPHP\Transformer\String\DigitsOnly;
+
+use PHPUnit\Framework\Attributes\CoversMethod;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+#[CoversMethod(DigitsOnlyTransformer::class, 'transform')]
+class DigitsOnlyTransformerTest extends TestCase
+{
+    #[Test]
+    public function removesNonAlphanumericCharacters(): void
+    {
+        $transformer = new DigitsOnlyTransformer();
+        $value = $transformer->transform("  A1Áβ٣\t\n\r\x00\x1F\x7F !@#-_ 　𐍈९");
+
+        $this->assertSame('1', $value);
+    }
+
+    #[Test]
+    public function returnsValueWhenInputIsNotString(): void
+    {
+        $transformer = new DigitsOnlyTransformer();
+        $value = $transformer->transform(123);
+
+        $this->assertSame(123, $value);
+    }
+}
